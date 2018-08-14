431daec128e630c90b8bfd474b6eac3da180affe
unknown <jayng168@gmail.com>
2018-08-14 02:15:46 -0400

    Updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,16 @@
-
+//The number of wins//
 var winCount = 0;
+//The number of loses//
 var loseCount = 0;
+//The display that will be shown on the HTML//
 var display = [];
+//The letters the player guessed//
 var playerGuess = [];
-var guessLeft = 9;
+//The number of guesses left//
+var guessLeft = 20;
 
+//The list of pokemon the computer can choose from//
+// var pokemonList = ["pok"];
 var pokemonList = ["Bulbasaur",
     "Ivysaur",
     "Venusaur",
@@ -157,23 +163,35 @@ var pokemonList = ["Bulbasaur",
     "Mewtwo",
     "Mew"];
 
-//Randomly picks a pokemon//    
-var computerGuess = pokemonList[Math.floor(Math.random() * pokemonList.length)];
+//For some reason I needed computerGuess to be a empty string for myRandom function to work// 
+var computerGuess = "";
 
-//Loop that pushes lines into an array equal the to the number of letters in the word//
-for (var i = 0; i < computerGuess.length; i++) {
-    display.push("_");
+//A function that randomly picks a pokemon//
+function myRandom() {
+    computerGuess = pokemonList[Math.floor(Math.random() * pokemonList.length)];
 };
 
-//Displays the array in HTML and turns it into a string//
-document.getElementById("poke").innerHTML = display.join(" ");
+//Activates the myRandom function to start the game with a random pokemon selected//
+myRandom();
+
+//A function that pushes lines into an array equal to the number of letters in the word//
+function myDisplay() {
+    for (var i = 0; i < computerGuess.length; i++) {
+        display.push("_");
+    };
+    //Displays the array in HTML and turns it into a string//
+    document.getElementById("poke").innerHTML = display.join(" ");
+};
+
+//Activates the myDisplay function to start the game with the selected pokemon with "_"//
+myDisplay();
 
 //Starts the game with a keyup//
 document.onkeyup = function (event) {
     var userGuess = event.key;
 
     //function that replaces the line in the array with the letter that was guessed if its correct//    
-    function myfunction() {
+    function myFunction() {
         for (var i = 0; i < computerGuess.length; i++) {
             if (userGuess === computerGuess[i]) {
                 display[i] = userGuess;
@@ -184,45 +202,48 @@ document.onkeyup = function (event) {
     };
 
     //To use the function when a button is pressed//
-    myfunction();
+    myFunction();
 
-    //Checks if the current guessed letter was in the already guessed letters array//
-    if (computerGuess.includes(userGuess)) {
-
+    //This If/Else statment checks if there is still any "_" left in the array "display"//
+    //I might need?(computerGuess.includes(userGuess)//
+    if (display.includes("_")) {
+        //If there is any "_" nothing happens//
     }
+    //If there isn't any "_" then the player wins the game. The answer is revealed, the winCount goes up by 1, the guessLeft and playerGuess is reset. Also the word is re-randomized and displayed with "_"//
+    else {
+        document.getElementById("results").innerHTML = computerGuess;
+        winCount += 1;
+        guessLeft = 20;
+        playerGuess = [];
+        display = [];
+        myRandom();
+        myDisplay();
+    };
 
-    else if (playerGuess.includes(userGuess)) {
-        results = "You lost!";
+    //This If/Else statment checks if the current userGuess was in the playerGuess//
+    if (playerGuess.includes(userGuess)) {
+        //If it is in playerGuess nothing happens//
     }
+    //If not then the player guessed wrong and guessLeft -1 and the word that was guessed is pushed into the playerGuess array//
     else {
         guessLeft -= 1;
-        results = "You lost!";
         playerGuess.push(userGuess);
     };
 
-    if (display.includes("_")) {
-
-    }
-    else {
-        winCount += 1;
-    }
-
+    //This If statment checks if the guessLeft is equal to 0. If so then the answer is revealed, the loseCount goes up by 1, the guessLeft and playerGuess is reset. Also the word is re-randomized and displayed with "_"//
     if (guessLeft === 0) {
         document.getElementById("results").innerHTML = computerGuess;
         loseCount += 1;
-        guessLeft = 5;
-        playerGuess = []
-        document.onkeyup = function (event) {
-            location.reload();
-        };
+        guessLeft = 20;
+        playerGuess = [];
+        display = [];
+        myRandom();
+        myDisplay();
     };
 
+    //This displays all the values//
     document.getElementById("wins").innerHTML = winCount;
     document.getElementById("loses").innerHTML = loseCount;
     document.getElementById("guessleft").innerHTML = guessLeft;
     document.getElementById("lettersleft").innerHTML = playerGuess;
-};
-
-if (guessLeft === 0) {
-    document.getElementById("results").innerHTML = computerGuess;
 };
