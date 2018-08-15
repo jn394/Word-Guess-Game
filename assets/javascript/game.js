df896cea94c2463f3d026ddb18958b3716559aed
unknown <jayng168@gmail.com>
2018-08-15 11:03:31 -0400

    Updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,174 +9,30 @@ var playerGuess = [];
 //The number of guesses left//
 var guessLeft = 20;
 
-//The list of pokemon the computer can choose from//
-// var pokemonList = ["pok"];
-var pokemonList = ["Bulbasaur",
-    "Ivysaur",
-    "Venusaur",
-    "Charmander",
-    "Charmeleon",
-    "Charizard",
-    "Squirtle",
-    "Wartortle",
-    "Blastoise",
-    "Caterpie",
-    "Metapod",
-    "Butterfree",
-    "Weedle",
-    "Kakuna",
-    "Beedrill",
-    "Pidgey",
-    "Pidgeotto",
-    "Pidgeot",
-    "Rattata",
-    "Raticate",
-    "Spearow",
-    "Fearow",
-    "Ekans",
-    "Arbok",
-    "Pikachu",
-    "Raichu",
-    "Sandshrew",
-    "Sandslash",
-    "Nidoran♀",
-    "Nidorina",
-    "Nidoqueen",
-    "Nidoran♂",
-    "Nidorino",
-    "Nidoking",
-    "Clefairy",
-    "Clefable",
-    "Vulpix",
-    "Ninetales",
-    "Jigglypuff",
-    "Wigglytuff",
-    "Zubat",
-    "Golbat",
-    "Oddish",
-    "Gloom",
-    "Vileplume",
-    "Paras",
-    "Parasect",
-    "Venonat",
-    "Venomoth",
-    "Diglett",
-    "Dugtrio",
-    "Meowth",
-    "Persian",
-    "Psyduck",
-    "Golduck",
-    "Mankey",
-    "Primeape",
-    "Growlithe",
-    "Arcanine",
-    "Poliwag",
-    "Poliwhirl",
-    "Poliwrath",
-    "Abra",
-    "Kadabra",
-    "Alakazam",
-    "Machop",
-    "Machoke",
-    "Machamp",
-    "Bellsprout",
-    "Weepinbell",
-    "Victreebel",
-    "Tentacool",
-    "Tentacruel",
-    "Geodude",
-    "Graveler",
-    "Golem",
-    "Ponyta",
-    "Rapidash",
-    "Slowpoke",
-    "Slowbro",
-    "Magnemite",
-    "Magneton",
-    "Farfetch'd",
-    "Doduo",
-    "Dodrio",
-    "Seel",
-    "Dewgong",
-    "Grimer",
-    "Muk",
-    "Shellder",
-    "Cloyster",
-    "Gastly",
-    "Haunter",
-    "Gengar",
-    "Onix",
-    "Drowzee",
-    "Hypno",
-    "Krabby",
-    "Kingler",
-    "Voltorb",
-    "Electrode",
-    "Exeggcute",
-    "Exeggutor",
-    "Cubone",
-    "Marowak",
-    "Hitmonlee",
-    "Hitmonchan",
-    "Lickitung",
-    "Koffing",
-    "Weezing",
-    "Rhyhorn",
-    "Rhydon",
-    "Chansey",
-    "Tangela",
-    "Kangaskhan",
-    "Horsea",
-    "Seadra",
-    "Goldeen",
-    "Seaking",
-    "Staryu",
-    "Starmie",
-    "Mr. Mime",
-    "Scyther",
-    "Jynx",
-    "Electabuzz",
-    "Magmar",
-    "Pinsir",
-    "Tauros",
-    "Magikarp",
-    "Gyarados",
-    "Lapras",
-    "Ditto",
-    "Eevee",
-    "Vaporeon",
-    "Jolteon",
-    "Flareon",
-    "Porygon",
-    "Omanyte",
-    "Omastar",
-    "Kabuto",
-    "Kabutops",
-    "Aerodactyl",
-    "Snorlax",
-    "Articuno",
-    "Zapdos",
-    "Moltres",
-    "Dratini",
-    "Dragonair",
-    "Dragonite",
-    "Mewtwo",
-    "Mew"];
 
-//For some reason I needed computerGuess to be a empty string for myRandom function to work// 
-var computerGuess = "";
+// var img = new Image();
+// img.src = "assets/javascript/jinx.png";
 
-//A function that randomly picks a pokemon//
+// document.body.appendChild(img);
+
+//Empty variables for my functions to push ojects from the array pokeList into// 
+var computerGuess;
+var compGuessName;
+var compGuessTypes;
+
+//A function that randomly picks a object from the array pokeList//
 function myRandom() {
-    computerGuess = pokemonList[Math.floor(Math.random() * pokemonList.length)];
+    computerGuess = pokeList[Math.floor(Math.random() * pokeList.length)];
+    //Once the object is selected, compGuessName pulls the ojects name//
+    compGuessName = computerGuess.pokeName.toLowerCase();
 };
 
 //Activates the myRandom function to start the game with a random pokemon selected//
 myRandom();
-
+console.log(pokeList.length);
 //A function that pushes lines into an array equal to the number of letters in the word//
 function myDisplay() {
-    for (var i = 0; i < computerGuess.length; i++) {
+    for (var i = 0; i < compGuessName.length; i++) {
         display.push("_");
     };
     //Displays the array in HTML and turns it into a string//
@@ -186,14 +42,23 @@ function myDisplay() {
 //Activates the myDisplay function to start the game with the selected pokemon with "_"//
 myDisplay();
 
+//A function that pulls the properties "Type 1" & "Type 2" from the object that was selected in the function myRandom//
+function myHint() {
+    compGuessTypes = computerGuess["Type 1"] + computerGuess["Type 2"];
+};
+
+//Activates the myHint function to start the game with a hint//
+myHint();
+
+
 //Starts the game with a keyup//
 document.onkeyup = function (event) {
     var userGuess = event.key;
 
     //function that replaces the line in the array with the letter that was guessed if its correct//    
     function myFunction() {
-        for (var i = 0; i < computerGuess.length; i++) {
-            if (userGuess === computerGuess[i]) {
+        for (var i = 0; i < compGuessName.length; i++) {
+            if (userGuess === compGuessName[i]) {
                 display[i] = userGuess;
             };
         };
@@ -204,6 +69,12 @@ document.onkeyup = function (event) {
     //To use the function when a button is pressed//
     myFunction();
 
+    //Once a new object is selected a new hint will appear//
+    myHint();
+
+    //Displays the hint in the HTML//
+    document.getElementById("hints").innerHTML = "Hints: " + compGuessTypes;
+
     //This If/Else statment checks if there is still any "_" left in the array "display"//
     //I might need?(computerGuess.includes(userGuess)//
     if (display.includes("_")) {
@@ -211,7 +82,7 @@ document.onkeyup = function (event) {
     }
     //If there isn't any "_" then the player wins the game. The answer is revealed, the winCount goes up by 1, the guessLeft and playerGuess is reset. Also the word is re-randomized and displayed with "_"//
     else {
-        document.getElementById("results").innerHTML = computerGuess;
+        document.getElementById("results").innerHTML = compGuessName;
         winCount += 1;
         guessLeft = 20;
         playerGuess = [];
@@ -232,7 +103,7 @@ document.onkeyup = function (event) {
 
     //This If statment checks if the guessLeft is equal to 0. If so then the answer is revealed, the loseCount goes up by 1, the guessLeft and playerGuess is reset. Also the word is re-randomized and displayed with "_"//
     if (guessLeft === 0) {
-        document.getElementById("results").innerHTML = computerGuess;
+        document.getElementById("results").innerHTML = compGuessName;
         loseCount += 1;
         guessLeft = 20;
         playerGuess = [];
